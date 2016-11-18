81695ddede60bddcd67ab2d228bb9e841453d77a
NaleRaphael <gmccntwxy@gmail.com>
2016-11-18 11:53:50 +0800

    rebuilt docs

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to sphinxdoc-test&#8217;s documentation!"],objects:{},titleterms:{welcom:0,indic:0,sphinxdoc:0,tabl:0,test:0,document:0}})
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index","includeme"],titles:["Welcome to sphinxdoc-test&#8217;s documentation!","&lt;no title&gt;"],objects:{},titleterms:{welcom:0,indic:0,sphinxdoc:0,tabl:0,test:0,document:0}})
